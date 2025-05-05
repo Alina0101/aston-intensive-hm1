42452fbc2ca0772f87dd72911c8de04d94e0ee92
Alina <kovaleva.allinna@gmail.com>
2025-05-06 01:58:05 +0300

    Initial commit

---
+++ src/task2/BadKey.java
@@ -0,0 +1,24 @@
+package task2;
+
+public class BadKey {
+    private final int id;
+
+    public BadKey(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 1; // Все ключи в один бакет
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return false; // Объекты никогда не равны
+    }
+
+    @Override
+    public String toString() {
+        return "BadKey{" + "id=" + id + '}';
+    }
+}
